86e56caa72c60ed8d57b723a550d5e71989bfdd1
Chandan Jog <chandanjog@Chandan-Jogs-MacBook-Pro.local>
2011-01-18 18:08:46 +0530

    <cj> trying out different backgrounds

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -3,6 +3,8 @@
   $(document).ready(function() {
 
     var selectedContributor = undefined;
+    var contributorDetailsSlider = $('#contributorDetailsSlider .content');  
+    var contributorDetailsClose = $('#contributorDetailsSlider .holder .close');
 
     $('li.contributor').each(function(index,contributor){
 
@@ -13,8 +15,9 @@
             if(selectedContributor === undefined){
                 self.addClass('selected');
                 self.find('.toggleImage').addClass('leftArrow');
-                $('#contributorDetailsSlider').append(self.find('.details').clone().show()).animate({ width: 'toggle'});
-                $('#contributorDetailsSlider').addClass('open');
+                contributorDetailsSlider.append(self.find('.details').clone().show()).animate({ width: 'toggle'});
+                contributorDetailsSlider.addClass('open');
+                contributorDetailsClose.show('slow');
             }
 
             if(selectedContributor  !== undefined && selectedContributor[0] != self[0]){
@@ -24,28 +27,29 @@
                 self.addClass('selected');
                 self.find('.toggleImage').addClass('leftArrow');
 
-                $('#contributorDetailsSlider').empty();
-                if($('#contributorDetailsSlider').hasClass('open')){
-                    $('#contributorDetailsSlider').append(self.find('.details').clone().show());
+                contributorDetailsSlider.empty();
+                if(contributorDetailsSlider.hasClass('open')){
+                    contributorDetailsSlider.append(self.find('.details').clone().show());
                 }
                 else{
-                    $('#contributorDetailsSlider').append(self.find('.details').clone().show()).animate({width: 'toggle'});
-                    $('#contributorDetailsSlider').addClass('open')
+                    contributorDetailsSlider.append(self.find('.details').clone().show()).animate({width: 'toggle'});
+                    contributorDetailsSlider.addClass('open')
+                    contributorDetailsClose.show('slow');
                 }
             }
 
             if(selectedContributor  !== undefined && selectedContributor[0] === self[0]){
-//                selectedContributor.removeClass('selected');
-//                selectedContributor.find('.toggleImage').removeClass('leftArrow');
 
-                $('#contributorDetailsSlider').animate({width: 'toggle'});
-                if($('#contributorDetailsSlider').hasClass('open')){
-                    $('#contributorDetailsSlider').removeClass('open');
+                contributorDetailsSlider.animate({width: 'toggle'});
+                if(contributorDetailsSlider.hasClass('open')){
+                    contributorDetailsSlider.removeClass('open');
+                    contributorDetailsClose.hide();
                     self.find('.toggleImage').removeClass('leftArrow');
                     self.removeClass('selected');
                 }
                 else{
-                    $('#contributorDetailsSlider').addClass('open');
+                    contributorDetailsSlider.addClass('open');
+                    contributorDetailsClose.show('slow');
                     self.find('.toggleImage').addClass('leftArrow');
                     self.addClass('selected');
                 }
@@ -72,13 +76,13 @@
     });
 
 
-    $('#contributorDetailsSlider').click(function(){
-        var self = $(this);
-        self.animate({width: 'toggle'});
+    contributorDetailsClose.click(function(){
+        contributorDetailsSlider.animate({width: 'toggle'});
         selectedContributor.removeClass('highlighted');
         selectedContributor.find('.toggleImage').removeClass('leftArrow');
         selectedContributor.removeClass('selected');
-        $('#contributorDetailsSlider').removeClass('open')        
+        contributorDetailsSlider.removeClass('open');
+        contributorDetailsClose.hide();
         
     });
 
